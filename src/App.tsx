6657d9a26eebbd4fb75e4f7beaa0b1fa44d00593
Fatoumata32 <f.ndiaye@alustudent.com>
2026-09-11 15:43:14 +0000

    Build React component with CSS styling

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import MyComponent from './MyComponent'
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <MyComponent />
+    </main>
+  )
+}
+
+export default App
